37f624721df1f3d60421028f7e2b47b181b8e1d2
MertAksu <mertaksu@outlook.com>
2018-09-07 22:24:18 +0300

    Cors filter added

--- RestAutomationAdmin-rest-services/src/main/java/com/rest/automation/admin/rest/authentication/SecurityConfiguration.java
+++ RestAutomationAdmin-rest-services/src/main/java/com/rest/automation/admin/rest/authentication/SecurityConfiguration.java
@@ -1,6 +1,8 @@
 package com.rest.automation.admin.rest.authentication;
 
+import com.sun.javafx.collections.ImmutableObservableList;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -8,6 +10,12 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import javax.validation.constraints.Null;
+import java.util.Arrays;
 
 @EnableWebSecurity
 @Configuration
@@ -22,8 +30,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		   http.csrf().disable().authorizeRequests().antMatchers(HttpMethod.POST,"/auth/login")
-		   .permitAll().anyRequest()
+		   http.cors().and().csrf().disable().authorizeRequests()
+           .antMatchers(HttpMethod.POST,"/auth/login").permitAll().anyRequest()
 		   .authenticated()
            .and()
            .httpBasic();
@@ -34,4 +42,22 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.authenticationProvider(provider);
 	}
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        final CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("HEAD",
+                "GET", "POST", "PUT", "DELETE", "PATCH"));
+        // setAllowCredentials(true) is important, otherwise:
+        // The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'.
+        configuration.setAllowCredentials(true);
+        // setAllowedHeaders is important! Without it, OPTIONS preflight request
+        // will fail with 403 Invalid CORS request
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 }
---
+++ RestAutomationAdmin-rest-services/src/main/java/com/rest/automation/admin/rest/authentication/WebConfiguration.java
@@ -0,0 +1,15 @@
+package com.rest.automation.admin.rest.authentication;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebConfiguration extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+    }
+}
